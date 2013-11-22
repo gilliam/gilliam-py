8ad73e71cae515015afd21d8ca25a7232a055909
Johan Rydberg <johan.rydberg@gmail.com>
2013-11-22 02:49:39 +0100

    start using versioneer

--- gilliam/__init__.py
+++ gilliam/__init__.py
@@ -16,3 +16,7 @@ from .executor import ExecutorClient
 from .builder import BuilderClient
 from .scheduler import SchedulerClient
 from .router import RouterClient
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
+import versioneer
+versioneer.versionfile_source = "gilliam/_version.py"
+versioneer.versionfile_build = "gilliam/_version.py"
+versioneer.tag_prefix = ""
+versioneer.parentdir_prefix = ""
+commands = versioneer.get_cmdclass().copy()
+
 setup(
     name='gilliam-py',
-    version='0.0',
+    version=versioneer.get_version(),
     packages=find_packages(),
     include_package_data=True,
     author='Johan Rydberg',
@@ -11,6 +18,7 @@ setup(
     description='Python bindings for the Gilliam platform',
     long_description=None,
     license='Apache 2.0',
+    cmdclass=commands,
     install_requires=[
         'requests >= 2.0',
         'python-circuit'
